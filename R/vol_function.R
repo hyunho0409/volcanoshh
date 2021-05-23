7e19f37b40f54d5dd6bcbde47c270b35613d0c72
donggrang <55303260+hyunho0409@users.noreply.github.com>
2021-05-23 22:20:10 +0900

    Update vol_function.R

--- R/vol_function.R
+++ R/vol_function.R
@@ -5,8 +5,6 @@ volcano_HH <- function(in_test = x1, color_range = c(x2,x3,x4), mycolors = c(x5,
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
-remove(list=ls())
-
 library(dplyr)
 library(readxl)
 library(ggplot2)
